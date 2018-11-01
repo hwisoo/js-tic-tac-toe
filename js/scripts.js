56efdaae278c61fe62bfd93e8632ba9473988c1e
Alex Williams and James Cho <james@>
2018-11-01 11:07:49 -0700

    Added functionality and win conditions

--- js/scripts.js
+++ js/scripts.js
@@ -49,17 +49,15 @@ BoardObject.prototype.assignCoords = function(space){
     } else 
         space.yCoord =1
     };
-// BoardObject.prototype.clickFill = function(space){
-//    space.filled = true;
-// }
+
 //Space Object
-function SpaceObject(filled, xCoord, yCoord){
+function SpaceObject(filled, current, xCoord, yCoord){
     this.filled = filled,
+    this.current = current,
     this.xCoord = xCoord,
     this.yCoord = yCoord
 
 }
-
 //Player Object 
 function PlayerObject(name, difficulty, marker){
     this.name = name,
@@ -67,9 +65,61 @@ function PlayerObject(name, difficulty, marker){
     this.marker = marker
 }
 
-//Click Logic
+//Win Logic
 
+BoardObject.prototype.updateSpace = function(clickid) {
+    //debugger;
 
+    if (clickid) {
+
+        var divClick = `${clickid}`;
+        var x = parseInt((clickid));
+        var node = document.getElementById(divClick)
+        boardObject.spaces[x - 1].filled = true;
+        if (node.textContent === "X") {
+            boardObject.spaces[x-1].current = "X";
+        } else if (node.textContent === "O") {
+            boardObject.spaces[x-1].current = "O";
+        }
+    };
+}
+
+BoardObject.prototype.winCheck = function() {
+    //debugger;
+    if (boardObject.spaces[0].current === "X" && boardObject.spaces[1].current === "X" && boardObject.spaces[2].current === "X") {
+        $("#winMessage").text("Player 1 Wins");
+    } else if (boardObject.spaces[3].current === "X" && boardObject.spaces[4].current === "X" && boardObject.spaces[5].current === "X") {
+        $("#winMessage").text("Player 1 Wins");
+    } else if (boardObject.spaces[6].current === "X" && boardObject.spaces[7].current === "X" && boardObject.spaces[8].current === "X") {
+        $("#winMessage").text("Player 1 Wins");
+    } else if (boardObject.spaces[0].current === "X" && boardObject.spaces[3].current === "X" && boardObject.spaces[6].current === "X") {
+        $("#winMessage").text("Player 1 Wins");
+    } else if (boardObject.spaces[1].current === "X" && boardObject.spaces[4].current === "X" && boardObject.spaces[7].current === "X") {
+        $("#winMessage").text("Player 1 Wins");
+    } else if (boardObject.spaces[2].current === "X" && boardObject.spaces[5].current === "X" && boardObject.spaces[8].current === "X") {
+        $("#winMessage").text("Player 1 Wins");
+    } else if (boardObject.spaces[0].current === "X" && boardObject.spaces[4].current === "X" && boardObject.spaces[8].current === "X") {
+        $("#winMessage").text("Player 1 Wins");
+    } else if (boardObject.spaces[2].current === "X" && boardObject.spaces[4].current === "X" && boardObject.spaces[6].current === "X") {
+        $("#winMessage").text("Player 1 Wins");
+    } else if (boardObject.spaces[0].current === "O" && boardObject.spaces[1].current === "O" && boardObject.spaces[2].current === "O") {
+        $("#winMessage").text("Player 2 Wins");
+    } else if (boardObject.spaces[3].current === "O" && boardObject.spaces[4].current === "O" && boardObject.spaces[5].current === "O") {
+        $("#winMessage").text("Player 2 Wins");
+    } else if (boardObject.spaces[6].current === "O" && boardObject.spaces[7].current === "O" && boardObject.spaces[8].current === "O") {
+        $("#winMessage").text("Player 2 Wins");
+    } else if (boardObject.spaces[0].current === "O" && boardObject.spaces[3].current === "O" && boardObject.spaces[6].current === "O") {
+        $("#winMessage").text("Player 2 Wins");
+    } else if (boardObject.spaces[1].current === "O" && boardObject.spaces[4].current === "O" && boardObject.spaces[7].current === "O") {
+        $("#winMessage").text("Player 2 Wins");
+    } else if (boardObject.spaces[2].current === "O" && boardObject.spaces[5].current === "O" && boardObject.spaces[8].current === "O") {
+        $("#winMessage").text("Player 2 Wins");
+    } else if (boardObject.spaces[0].current === "O" && boardObject.spaces[4].current === "O" && boardObject.spaces[8].current === "O") {
+        $("#winMessage").text("Player 2 Wins");
+    } else if (boardObject.spaces[2].current === "O" && boardObject.spaces[4].current === "O" && boardObject.spaces[6].current === "O") {
+        $("#winMessage").text("Player 2 Wins");
+    };
+}
 
 
 
@@ -78,58 +128,62 @@ var gameObject = new GameObject();
 var boardObject = new BoardObject();
 
 $(document).ready(function() {
-    counter = 1;
-    $("div.clickable").click(function(e){
-        var click = e.target;
-       
-        console.log(click.id);
-        if(click && counter%2 ==0 && $(`div#${click.id}`).hasClass("clickable") === true) {
-            
-            $(`div#${click.id}`).text("X");
-            $(`div#${click.id}`).removeClass("clickable");
-            
-        } else if(click && counter%2 !=0 && $(`div#${click.id}`).hasClass("clickable") === true){
-            
-            $(`div#${click.id}`).text("O");
-            $(`div#${click.id}`).removeClass("clickable");
-        }
-        counter++;
 
-    });
-   
     $("#inputButton").click(function(event) {
         event.preventDefault();
         var playerOne = $("input#player1").val();
+        var playerTwo = $("input#player1").val();
         var playerDifficulty = $("select#difficulty").val();
         var filled = false;
+        var current = "";
         var xCoord = 0;
         var yCoord = 0;
         var marker = "";
-        var newPlayer = new PlayerObject(playerOne, playerDifficulty, marker);
-        gameObject.addPlayer(newPlayer);
+        
+        var newPlayerOne = new PlayerObject(playerOne, playerDifficulty, marker);
+        gameObject.addPlayer(newPlayerOne);
+        var newPlayerTwo = new PlayerObject(playerTwo, playerDifficulty, marker);
+        gameObject.addPlayer(newPlayerTwo);
         
         //debugger;
         for(var i =0; i < 9; i++){
-            var space = new SpaceObject(filled, xCoord, yCoord)
+            var space = new SpaceObject(filled, current, xCoord, yCoord)
             boardObject.addSpaces(space);
             boardObject.assignCoords(space);
-            // if (space.id <= 3) {
-            //     $("#row1").append("<div id='"+ space.id + "' class='col col-md-4'></div>");
-                
-            // } else if (space.id <=6 ) {
-            //     $("#row2").append("<div id='"+ space.id + "' class='col col-md-4'></div>");
-                    
-                               
-            // } else {
-            //     $("#row3").append("<div id='"+ space.id + "' class='col col-md-4'></div>");         
-            // };
         };
-        
-
+       
         $("form").hide();
-        
+        $("#board").show();
         console.log(boardObject);
         
+        
+    });
+
+    counter = 0;
+    
+    $("div.clickable").click(function(e){
+        var click = e.target;
+        
+
+        console.log(click.id);
+        if ($(`div#${click.id}`).hasClass("clickable") === true) {
+            if(click && counter%2 ==0 ) {
+                
+                $(`div#${click.id}`).text("X");
+                $(`div#${click.id}`).removeClass("clickable");
+                counter++;
+                boardObject.updateSpace(click.id);
+                boardObject.winCheck();
+                
+            } else if(click && counter%2 !=0 ){
+                
+                $(`div#${click.id}`).text("O");
+                $(`div#${click.id}`).removeClass("clickable");
+                counter++;
+                boardObject.updateSpace(click.id);
+                boardObject.winCheck();
+            }
+        };
         
     });
     
